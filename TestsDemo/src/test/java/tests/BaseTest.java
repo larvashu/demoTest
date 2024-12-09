6b954adcb4f3c20c57c7791e623f07cc1407a8e0
larvashu <kris.galinski@gmail.com>
2024-12-09 03:41:23 +0100

    cleanup, update README.md

--- TestsDemo/src/test/java/tests/BaseTest.java
+++ TestsDemo/src/test/java/tests/BaseTest.java
@@ -2,22 +2,14 @@ package tests;
 
 import baseClasses.BaseReport;
 import extensions.ScreenShotOnFailure;
-import io.qameta.allure.Allure;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.TestWatcher;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import utils.DriverFactory;
-
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
-@ExtendWith(ScreenShotOnFailure.class) // Apply the screenshot extension here
+@ExtendWith(ScreenShotOnFailure.class)
 public abstract class BaseTest extends BaseReport {
 
     protected static WebDriver driver;
@@ -30,6 +22,7 @@ public abstract class BaseTest extends BaseReport {
     @AfterAll
     public static void teardown() throws IOException {
         DriverFactory.quitDriver();
-        displayRaport();
+        //displayRaport();
+        //TODO: Nalezy odkomentowac powyzsza linie, zeby reporty wlaczaly sie automatycznie (1 zakladka per testKlasa)
     }
 }
